0090b7fbeea4fc1cce3f28f3ed0f3f255312e675
choigwangho <cgh-_@naver.com>
2020-08-11 21:28:25 +0900

    commit

---
+++ src/main/java/com/covid19/app/admin/login/service/AdminService.java
@@ -0,0 +1,15 @@
+package com.covid19.app.admin.login.service;
+
+import com.covid19.app.admin.login.vo.AdminMember;
+
+public interface AdminService {
+
+	public AdminMember selectAdmin(AdminMember admember);
+
+
+
+	
+	
+
+
+}
