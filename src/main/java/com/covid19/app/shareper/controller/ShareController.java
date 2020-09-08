bd2873ed3dfb645bcbe79f4c40738ff58ec38acd
ats3059 <ats3059@naver.com>
2020-09-08 21:44:44 +0900

    commit

--- src/main/java/com/covid19/app/shareper/controller/ShareController.java
+++ src/main/java/com/covid19/app/shareper/controller/ShareController.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
-import java.lang.ProcessBuilder.Redirect;
-
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.List;
